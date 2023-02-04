82ee2a7577f8c1ab9aebaa149b8d1b5ff8c3e705
StEugen <85479518+StEugen@users.noreply.github.com>
2023-02-04 14:30:35 +0400

    Login made
    Login system made
    Registration to make

--- app/volunteer/urls.py
+++ app/volunteer/urls.py
@@ -7,4 +7,6 @@ from volunteer.views import MainView
 app_name = 'volunteer'
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('registration', views.user_registration, name='registration'),
+    path('login', views.user_login, name='login'),
 ]
--- app/volunteer/views.py
+++ app/volunteer/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseForbidden, HttpResponse
 from volunteer.models import Dashboards, Volunteers, User, Blogpost
 from django.views.generic import View, TemplateView, ListView
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 
 class MainView(TemplateView):
@@ -13,3 +15,21 @@ class MainView(TemplateView):
             'posts': posts,
         }
         return context
+
+
+def user_registration(request):
+    pass
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, 
+                            username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('volunteer:index')
+        else:
+            messages.success(request, ("There was a problem"))
+            return redirect('volunteer:login')
+    return render(request, 'login.html')
